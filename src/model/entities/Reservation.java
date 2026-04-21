630670a03dca0c39db3886c1c93533fbeeefe14f
Dario Monteiro <dlbrmonteiro@gmail.com>
2026-04-21 12:42:33 -0300

    Solucao boa com excecao personalizada extendendo Exception

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	private Integer roomNumber;
 	private Date checkIn;
@@ -14,7 +16,10 @@ public class Reservation {
 	public Reservation() {		
 	}
 
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException{
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");				
+		}		
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -41,13 +46,13 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff,TimeUnit.MILLISECONDS);   // converte milisegundos em dias
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {		
+	public void updateDates(Date checkIn, Date checkOut) throws DomainException {		
 		Date now = new Date();
 		if (checkIn.before(now) || checkOut.before(now)) {
-			throw  new IllegalArgumentException("Reservation dates for update must be future dates");  // essa exceção é utilizada quando existe erro na passagem de argumnento
+			throw  new DomainException("Reservation dates for update must be future dates");  // essa exceção é utilizada quando existe erro na passagem de argumnento
 		}
 		if (!checkOut.after(checkIn)) {
-			throw new IllegalArgumentException("Check-out date must be after check-in date");				
+			throw new DomainException("Check-out date must be after check-in date");				
 		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
